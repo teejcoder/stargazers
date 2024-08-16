4f1612e2aef72f79093daef15d06bc92c3b7a017
teejcoder <96551689+teejcoder@users.noreply.github.com>
2024-08-16 22:37:07 +1000

    minor styling fixes

--- src/components/GetImage.js
+++ src/components/GetImage.js
@@ -36,7 +36,7 @@ function GetImage() {
       </div>
       <Particles
         quantity={500}
-        className="absolute top-0 left-0 w-full h-full -z-10"
+        className="absolute w-full h-full -z-10"
       />
         {
           !pictureOfTheDay ? 
